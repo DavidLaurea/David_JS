3ba559b9b31526e30b70fac36571d7738c86ced2
DavidLaurea <146191168+DavidLaurea@users.noreply.github.com>
2024-09-10 23:25:08 +0300

    Delete Workshop2 directory

--- Workshop2/Ex2.js
+++
@@ -1,7 +0,0 @@
-document.write("<p>Exercise 2</p>");
-function repeatingText() {
-    for (let i = 0; i < 50; i++) {
-        document.write('line of text ' + (i + 1) + '<br>');
-    }
-}
-repeatingText();
--- Workshop2/js/Ex3.js
+++
@@ -1,6 +0,0 @@
-if (navigator.userAgent.includes("Firefox")) {
-    window.location.href = "http://www.mozilla.org";
-} 
-else {
-            window.location.href = "https://www.google.com";
-}
